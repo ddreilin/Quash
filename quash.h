0157d50db731a1d8bad954b9e66ce50cb555b6fd
ddreilin <d727d733@ku.edu>
2016-03-01 19:47:27 -0600

    Doxygen comments

--- quash.h
+++ quash.h
@@ -5,12 +5,6 @@
 //
 //*********************************************************
 
-//********************************************************* 
-//
-//! \addtogroup quash.h
-//! @{
-//
-//*********************************************************
 #ifndef QUASH_H
 #define QUASH_H
 
@@ -23,15 +17,14 @@
 // Specify the maximum number of characters accepted by the command string
 //
 //*****************************************************************************
-
-
 #define MAX_COMMAND_LENGTH (1024)
+
+
 //*****************************************************************************
 //
 //  Holds information about a command.
 //
 //*****************************************************************************
-
 typedef struct command_t {
   char cmdstr[MAX_COMMAND_LENGTH]; ///< character buffer to store the
                                    ///< command string. You may want
@@ -49,15 +42,13 @@ typedef struct command_t {
 // @return True if Quash should accept more input and false otherwise
 //
 //*****************************************************************************
-
-
 bool is_running();
+
 //*****************************************************************************
 //
 // Causes the execution loop to end.
 //
 //*****************************************************************************
-
 void terminate();
 
 /**
@@ -71,15 +62,59 @@ void terminate();
  */
 bool get_command(command_t* cmd, FILE* in);
 
-//Manages what to run
+/**
+ *  Read in a command and parse the input for executing.
+ *
+ *  @param cmd - a command_t structure.
+ *  @return void
+ */
 void manager( command_t* cmd );
 
-// run commands functions
+/**
+ *  Changes current working directory of user. 
+ *
+ *  @param tokens - an absolute file path to go to. If NULL, current working directory goes home
+ *  @return void
+ */
 void run_cd( char* tokens );
+
+/**
+ *  Set PATH to a new value
+ *
+ *  @param tokens - new value for PATH variable
+ *  @return void
+ */
 void run_set( char* tokens );
+
+/**
+ *  Prints value for HOME or PATH variable,
+ *
+ *  @param tokens - string to be echoed
+ *  @return void
+ */
 void run_echo( char* tokens );
-void run_pwd( char* tokens );
+
+/**
+ *  Prints current working directory
+ *
+ *  @param none
+ *  @return void
+ */
+void run_pwd();
+
+/**
+ * Print all of the currently running background processes in the format:[JOBID] PID COMMAND where JOBID is a
+ * unique positive integer quash assigns
+ * to the job to identify it, PID is the PID of the child process used for the job, and
+ * COMMAND is the command used to invoke the job.
+ *
+ *  @param cmd - a command_t structure.
+ *   
+ *  @return void
+ */
 void run_jobs( command_t* cmd, char* tokens );
+
+
 void run_exec( command_t* cmd, char* tokens );
 
 //exec helper functions
