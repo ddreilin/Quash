41921c6287e07afec6213726430eb8b71123c1bc
ddreilin <d727d733@ku.edu>
2016-03-01 19:44:21 -0600

    Began adding doxygen style comments

--- quash.h
+++ quash.h
@@ -1,9 +1,16 @@
-/**
- * @file quash.h
- *
- * Quash essential functions and structures.
- */
+//*********************************************************
+//@file quash.h
+// 
+//Quash essential functions and structures.
+//
+//*********************************************************
 
+//********************************************************* 
+//
+//! \addtogroup quash.h
+//! @{
+//
+//*********************************************************
 #ifndef QUASH_H
 #define QUASH_H
 
@@ -11,15 +18,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+//*****************************************************************************
+//
+// Specify the maximum number of characters accepted by the command string
+//
+//*****************************************************************************
 
-/**
- * Specify the maximum number of characters accepted by the command string
- */
+
 #define MAX_COMMAND_LENGTH (1024)
+//*****************************************************************************
+//
+//  Holds information about a command.
+//
+//*****************************************************************************
 
-/**
- * Holds information about a command.
- */
 typedef struct command_t {
   char cmdstr[MAX_COMMAND_LENGTH]; ///< character buffer to store the
                                    ///< command string. You may want
@@ -31,16 +43,21 @@ typedef struct command_t {
   // Extend with more fields if needed
 } command_t;
 
-/**
- * Query if quash should accept more input or not.
- *
- * @return True if Quash should accept more input and false otherwise
- */
-bool is_running();
+//*****************************************************************************
+//
+// Query if quash should accept more input or not.
+// @return True if Quash should accept more input and false otherwise
+//
+//*****************************************************************************
 
-/**
- * Causes the execution loop to end.
- */
+
+bool is_running();
+//*****************************************************************************
+//
+// Causes the execution loop to end.
+//
+//*****************************************************************************
+
 void terminate();
 
 /**
